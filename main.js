062399ef1bfa702bd5cc0b8cd70a45fa95832cd7
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-06-19 18:02:34 +0100

    :fire: :art: Add Slider Func :sparkles: :globe_with_meridians:

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const slides = document.querySelectorAll('.product_Slider');
+const slideImage = document.querySelector('.hero_left');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
+var bgSlides = ["./images/desktop-image-hero-1.jpg", "./images/desktop-image-hero-2.jpg", "./images/desktop-image-hero-3.jpg"];
 
 const nextSlide = () => {
   // Get current class
@@ -34,11 +36,40 @@ const prevSlide = () => {
   setTimeout(() => current.classList.remove('current'));
 };
 
+var bgIndex = 0;
+const nextImage = () => {
+  if (bgIndex < bgSlides.length - 1) {
+    bgIndex++;
+    slideImage.style.background = `url(${bgSlides[bgIndex]}) no-repeat center center/cover`;
+    console.log(bgIndex);
+    console.log(bgSlides.length);
+    console.log(bgSlides[bgIndex]);
+  } else {
+    bgIndex = 0;
+    slideImage.style.background = `url(${bgSlides[bgIndex]}) no-repeat center center/cover`;
+  }
+};
+
+const prevImage = () => {
+  if (bgIndex < bgSlides.length && bgIndex > 0) {
+    bgIndex--;
+    slideImage.style.background = `url(${bgSlides[bgIndex]}) no-repeat center center/cover`;
+    console.log(bgIndex);
+    console.log(bgSlides.length);
+    console.log(bgSlides[bgIndex]);
+  } else {
+    bgIndex = 2;
+    slideImage.style.background = `url(${bgSlides[bgIndex]}) no-repeat center center/cover`;
+  }
+};
+
 // Button events
 next.addEventListener('click', () => {
   nextSlide();
+  nextImage();
 });
 
 prev.addEventListener('click', () => {
   prevSlide();
+  prevImage();
 });
